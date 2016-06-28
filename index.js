1a49c08a6b42f45b79cd8fc0f27c409b6d9977c2
Michael <email": null>
2016-06-28 15:57:48 +1200

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var extraData = require('./views/extraData.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
+
+// function updateData () {
 xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
+    if (err) {
+      console.error(err)
+    }
+    // In case you're curious
+    console.log(data.body) // FYI: data.body is a string
 
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+    // Replace 'Space' below with the response
+    var target = document.getElementsByTagName('main')[0]
+    var obj = JSON.parse(data.body)
+    target.innerHTML = greeting(obj)
+  })
 
-  // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  var obj = JSON.parse(data.body)
-  target.innerHTML = greeting(obj)
-})
+document.getElementById("update").addEventListener("click", updatePage);
+
+function updatePage () {
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
+    // In case you're curious
+    console.log(data.body) // FYI: data.body is a string
+
+    // Replace 'Space' below with the response
+    var target = document.getElementsByTagName('second')[0]
+    var obj = JSON.parse(data.body)
+    target.innerHTML = extraData(obj)
+  })
+}
+
+
+
+
+
